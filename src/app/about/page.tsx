d65acf312ee819dd13b17f57dcd4db04eeb58a06
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-07-04 22:02:20 -0300

    componentizing cards for the page and improving the card style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,74 +1,62 @@
 'use client'
 
 import Page from '@/components/Page'
-import RotatingLines from '@/components/RotatingLines'
+import ContainerMotion from '@/components/ContainerMotion'
 
 import { Press_Start_2P } from 'next/font/google'
-import { Github, Linkedin, Mail } from 'lucide-react'
-import { motion } from 'framer-motion'
+import { Github, Linkedin, Mails } from 'lucide-react'
 
 const pressStart2P = Press_Start_2P({
   subsets: ['latin'],
   weight: '400',
 })
 
+const Links = [
+  {
+    icon: <Github className="w-5 h-5" />,
+    href: 'https://github.com/leandroxzq',
+    span: 'leandroxzq',
+  },
+  {
+    icon: <Linkedin className="w-5 h-5" />,
+    href: 'https://www.linkedin.com/in/leandrosilvaz/',
+    span: 'leandrosilvaz',
+  },
+  {
+    icon: <Mails className="w-5 h-5" />,
+    href: '',
+    span: 'leandrosilvazw25@gmail.com',
+  },
+]
+
 export default function About() {
   return (
     <Page>
-      <div className="space-y-8">
-        <motion.section
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="relative dark:bg-gray-900 p-6 rounded-lg shadow-md overflow-hidden"
-        >
-          <RotatingLines />
-          <div className="relative z-10">
-            <h3 className={`text-xl font-semibold mb-4 ${pressStart2P.className}`}>About Me</h3>
-            <p className="leading-relaxed">About me</p>
-          </div>
-        </motion.section>
+      <ContainerMotion>
+        <h3 className={`text-xl font-bold text-white mb-4 ${pressStart2P.className}`}>About Me</h3>
+        <p className="leading-relaxed">
+          My name is Leandro, and I am currently studying Systems Analysis and Development at IFPE.
+          I am dedicated, focused on continuous learning, with the goal of working as a fullstack
+          professional.
+        </p>
+      </ContainerMotion>
 
-        <motion.section
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.3 }}
-          className="relative bg-white dark:bg-gray-900 p-6 rounded-lg shadow-md overflow-hidden"
-        >
-          <RotatingLines />
-          <div className="relative z-10">
-            <h3
-              className={`text-xl font-semibold text-gray-800 dark:text-white mb-4 ${pressStart2P.className}`}
-            >
-              Contact
-            </h3>
-            <div className="space-y-3">
-              <a
-                className="flex items-center gap-3 text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
-                href="https://github.com/leandroxzq"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Github className="w-5 h-5" />
-                <span>leandroxzq</span>
-              </a>
-              <a
-                className="flex items-center gap-3 text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors"
-                href="https://www.linkedin.com/in/leandrosilvaz/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Linkedin className="w-5 h-5" />
-                <span>leandrosilvaz</span>
-              </a>
+      <ContainerMotion delay={0.3}>
+        <h3 className={`text-xl font-bold text-white mb-4 ${pressStart2P.className}`}>Contact</h3>
 
-              <p className="flex gap-3 text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition-colors">
-                <Mail className="w-5 h-5" />
-                <span>leandrosilvazw25@gmail.com</span>
-              </p>
-            </div>
-          </div>
-        </motion.section>
-      </div>
+        {Links.map((link, index) => (
+          <a
+            key={index}
+            className="flex w-fit p-2 gap-2 font-semibold rounded-md text-black bg-gray-300 hover:text-blue-600 transition-colors mb-3"
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {link.icon}
+            <span>{link.span}</span>
+          </a>
+        ))}
+      </ContainerMotion>
     </Page>
   )
 }
---
+++ src/components/ContainerMotion.tsx
@@ -0,0 +1,16 @@
+import { motion } from 'framer-motion'
+
+export default function ContainerMotion({ children, delay = 0 }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay }}
+      className={
+        'relative border-2 border-gray-500 bg-gray-900 p-6 rounded-lg shadow-md overflow-hidden bg-gradient'
+      }
+    >
+      {children}
+    </motion.div>
+  )
+}
