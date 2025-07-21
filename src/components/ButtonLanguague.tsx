dbf6132e791e95d5ff1b7cb3354a2ef68b305cbf
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-07-20 21:47:39 -0300

    button improvements ui and responsive

--- src/components/ButtonLanguague.tsx
+++ src/components/ButtonLanguague.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import { motion } from "framer-motion"
 import { Select } from '@mantine/core'
 
 import { useLanguage } from '@/context/LanguageContext'
@@ -5,25 +9,46 @@ import { useLanguage } from '@/context/LanguageContext'
 export default function ButtonLanguage() {
   const { language, changeLanguage } = useLanguage()
 
+  const [screenSize, setScreenSize] = useState({ width: 0, height: 0 })
+
+  useEffect(() => {
+    const updateSize = () =>
+      setScreenSize({ width: window.innerWidth, height: window.innerHeight })
+
+    updateSize()
+    window.addEventListener('resize', updateSize)
+
+    return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  const isDesktop = screenSize.width >= 768
+
   return (
-    <Select
-      classNames={{
-        root: 'text-white',
-        wrapper: 'bg-[#121212] border border-gray-700 rounded-md',
-        input: 'bg-[#121212] text-white border-none focus:ring-2 focus:ring-yellow-300 rounded-md',
-        dropdown: 'bg-[#1a1a1a] border border-gray-700 text-white',
-        option: 'bg-[#1a1a1a] text-white hover:bg-gray-700',
-      }}
-      value={language}
-      onChange={(value) => {
-        if (value) changeLanguage(value)
-      }}
-      data={[
-        { value: 'en', label: 'English' },
-        { value: 'pt', label: 'Português' },
-      ]}
-      defaultValue="en"
-      className="w-48"
-    />
+    <motion.div 
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.7 }}
+    >
+      <Select
+        classNames={{
+          root: 'text-white',
+          wrapper: 'bg-[#121212] border border-gray-200 rounded-md',
+          input: 'bg-[#121212] text-white border-none focus:ring-2 focus:ring-yellow-300 rounded-md',
+          dropdown: 'bg-[#1a1a1a] border border-gray-200 text-white',
+          option: 'bg-[#1a1a1a] text-white hover:bg-gray-700',
+        }}
+        value={language}
+        onChange={(value) => {
+          if (value) changeLanguage(value)
+        }}
+        data={[
+          { value: 'en', label: isDesktop ? 'English' : '🇺🇸' },
+          { value: 'pt', label: isDesktop ? 'Português' : '🇧🇷' },
+        ]}
+        withCheckIcon={isDesktop ? true : false}
+        checkIconPosition="right"
+        className={isDesktop ? "w-36 absolute" : "w-16 absolute top-4 right-4"}
+      />
+    </motion.div>
   )
 }
