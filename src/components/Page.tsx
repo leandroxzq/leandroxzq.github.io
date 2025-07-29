47dbc81f2d8712a3b124a3a7554bc647a416bc01
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-07-29 00:13:24 -0300

    margin on layout page

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -15,7 +15,7 @@ export default function Page({ children }) {
     <main className="flex min-h-dvh flex-col items-center lg:flex-row lg:items-start">
       <Nav showBackButton excludeCurrentLink showPathname></Nav>
       <div
-        className={`max-h-[90dvh] w-full max-w-3xl space-y-8 rounded-md sm:w-10/12 sm:px-6 md:max-h-[95dvh] lg:px-8 ${isProjects ? 'overflow-hidden px-2' : 'custom-scroll overflow-auto'}`}
+        className={`mt-3 max-h-[90dvh] w-full max-w-3xl space-y-8 rounded-md sm:w-10/12 sm:px-6 md:max-h-[95dvh] lg:px-8 ${isProjects ? 'overflow-hidden px-2' : 'custom-scroll overflow-auto'}`}
       >
         {children}
       </div>
