de3bba5a475580f760e06b29853e1de8fddd0c5b
Leandro <149272684+leandroxzq@users.noreply.github.com>
2025-08-28 21:00:10 -0300

    fix build next

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import Nav from '@/components/Nav'
 export default function Blog() {
   return (
     <main className="flex min-h-screen select-none pt-3 md:pt-7">
-      <Nav showBackButton excludeCurrentLink showPathname></Nav>
+      <Nav showBackButton showPathname></Nav>
       <div className="w-10/12"></div>
     </main>
   )
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,7 +3,7 @@ import Nav from '@/components/Nav'
 export default function Page({ children }) {
   return (
     <main className="flex min-h-dvh flex-col items-center lg:flex-row lg:items-start">
-      <Nav showBackButton excludeCurrentLink showPathname></Nav>
+      <Nav showBackButton showPathname></Nav>
       <div
         className={`custom-scroll mt-4 max-h-[90dvh] w-full max-w-3xl space-y-8 overflow-auto rounded-md sm:w-10/12 sm:px-6 md:max-h-[96dvh] lg:px-8`}
       >
